f3295c6a9e16272296d6a591422cb634c3c52f21
Arkadii <arkadii.malygin@gmail.com>
2024-11-23 00:41:40 +0200

    different test implementation

--- build.gradle.kts
+++ build.gradle.kts
@@ -56,7 +56,7 @@ dependencies {
 	testImplementation("org.springframework.security:spring-security-test")
 	testImplementation(platform("org.junit:junit-bom:5.10.1"))
 	testImplementation("org.junit.jupiter:junit-jupiter:5.10.1")
-	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+	testImplementation("org.junit.platform:junit-platform-launcher")
 
 }
 
