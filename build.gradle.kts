1b0d4f484fe7ec59936ca142db6876ece571e6b7
Arkadii <arkadii.malygin@gmail.com>
2024-11-22 22:09:53 +0200

    test runtime

--- build.gradle.kts
+++ build.gradle.kts
@@ -56,7 +56,7 @@ dependencies {
 	testImplementation("org.springframework.security:spring-security-test")
 	testImplementation(platform("org.junit:junit-bom:5.10.1"))
 	testImplementation("org.junit.jupiter:junit-jupiter:5.10.1")
-	//testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 
 }
 
