5dc4a7d8394a743943cc92a05184817aeafe72fb
ArkadiiMalygin <160322297+ArkadiiMalygin@users.noreply.github.com>
2024-11-25 14:25:55 +0200

    Update Task.java

--- src/main/java/hexlet/code/app/model/Task.java
+++ src/main/java/hexlet/code/app/model/Task.java
@@ -22,7 +22,7 @@ import lombok.ToString;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import java.time.LocalDate;
+import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -73,7 +73,7 @@ public class Task {
     private List<Label> labels = new ArrayList<>();
 
     @CreatedDate
-    private LocalDate createdAt;
+    private Instant createdAt;
 
     public void setLabels(List<Label> labelsToAdd) {
 //        labelsToAdd.forEach(l -> l.addTask(this));
